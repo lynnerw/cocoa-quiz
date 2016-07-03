567fb2931b82fa30fb9c0642163bb40e290eb15e
lynnerw <larwitter@gmail.com>
2016-07-03 17:06:14 -0400

    js is displaying Q and A but not with style and not waiting for user click

--- js/quiz.js
+++ js/quiz.js
@@ -1,27 +1,45 @@
 $(document).ready(function(){
 
-/* on click within the ordered list, hide the Q and A and show correctAns */
-$('li' ).on('click', function() {
-  console.log("user selected answer #");
-    $(this).toggle('.flipped', '.back');
-});
-
-/* quiz is array of question and answer pairs */
-
-var quiz = {
-    Q:["Cocoa is...", "People have been enjoying cocoa since ", "Before cocoa beans can be used for chocolate they are ", "The following statments about chocolate consumption are true: ", "The following benefits of cocoa have been studied: "]
-    A:[]
-    correctA:[4, 3, 5, 5, 5]
-}
-
 var score = 0;
 
-for (i = 0; i < 5, i++)
+var currentIndex = 0;
 
-    /* quiz.Q is an array of single strings; quiz.A is an array of ordered list */
+var quiz = {
+    Q:["<p>Cocoa is...</p>", "<p>People have been enjoying cocoa since </p>", "<p>To prepare cocoa beans for making chocolate they are </p>", "<p>The following statments about chocolate consumption are true: </p>", "<p>The following benefits of cocoa have been studied: </p>"],
+    A:[["<li>A small town in Florida.</li>", "<li>An ingredient required to make chocolate</li>", "<li>Apple’s object­-oriented API for OS X.</li>", "<li>All of the above.</li>"], ["<li>The height of the Roman empire, circa 100 A.D.</li>", "<li>Europeans found it in South America in 1517.</li>",
+    "<li>The middle of the Late Bronze Age, about 2000 B.C.</li>", "<li>The Hershey family started making chocolate in 1879.</li>"]],
+    correctA:[3, 2, 4, 4, 4]
+};
+
+function displayQandA() {
+    $(quiz.Q[currentIndex] + quiz.A[currentIndex]).appendTo('.quizCard');
+}
+displayQandA(currentIndex);
+
+$('.quizCard').click(function() {
+   var userAnswer = $(this).parent('li');
+});
+
+/*  $('input[type="submit"]').click(function() { */
+       // assign text input to a variable
+/*    var textInput = $('input[name="new-area-item"]', $(this).parent('form') );
+       var newItem = textInput.val();
+*/
+
+console.log(quiz.Q[currentIndex]);
+console.log("userAnswer is " + quiz.Q[currentIndex]);
+
+$('.nextQuestion').click(function() {
+    currentIndex++;
+});
+
+
+    /* quiz.Q is an array of single strings; quiz.A is an array of ordered list
     $('quizCard').html(quiz.Q[i] + quiz.A[i])
 
-    input userAnswer (userAnswer = onclick # of li)
+/* subarrary is li tag with an index - append to HTML
+    (userAnswer = onclick # of li - checking
+    $this([i]) - like shopping listdetect user click
 
     $('quizCard').hide(quizQ[i], quizA[i])
 
@@ -32,14 +50,15 @@ for (i = 0; i < 5, i++)
         else {
             prepend( "Oops. ") to quizA[i].correctA
         },
-    display quizA[i].correctA on quizCard,
+    $('quizCard').html(quizA[i].correctA)
     display funFact card,
-    display nextQ button,
-    on button click, hide(quizA[i].correctA and funFact)
 
- end for loop */
+    display nextQ button,
+    pause - on button click, hide(quizA[i].correctA and funFact)
+
+ end for loop
 
 display on quizCard ("Your score was " + score + " out of 5.")
-
+*/
 
 });
