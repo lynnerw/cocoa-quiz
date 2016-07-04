945b950408c94255468717efa8309cc29d586734
lynnerw <larwitter@gmail.com>
2016-07-03 20:08:33 -0400

    minor improvement, on click still not working

--- js/quiz.js
+++ js/quiz.js
@@ -8,7 +8,9 @@ var quiz = {
     Q:["<p>Cocoa is...</p>", "<p>People have been enjoying cocoa since </p>", "<p>To prepare cocoa beans for making chocolate they are </p>", "<p>The following statments about chocolate consumption are true: </p>", "<p>The following benefits of cocoa have been studied: </p>"],
     A:[["<li>A small town in Florida.</li>", "<li>An ingredient required to make chocolate</li>", "<li>Apple’s object­-oriented API for OS X.</li>", "<li>All of the above.</li>"], ["<li>The height of the Roman empire, circa 100 A.D.</li>", "<li>Europeans found it in South America in 1517.</li>",
     "<li>The middle of the Late Bronze Age, about 2000 B.C.</li>", "<li>The Hershey family started making chocolate in 1879.</li>"]],
-    correctAns:[3, 2, 4, 4, 4]
+    correctAns:[3, 2, 4, 4, 4],
+    funFact:["<p><strong>Fun Fact:</strong> Cocoa comes from the tiny nib inside the cocoa seed(bean). There are 20-50 seeds inside a pod and a cocoa tree produces 5-7 pods annually.</p>", "<p><strong>Fun Fact:</strong> ​Cocoa beans were so valuable to the Aztec culture that they used them as currency; one fresh avocado was worth three beans.</p>", "                <p><strong>Fun Fact:</strong> ​Chocolate gives you a more intense mental high and gets your heart pounding more than kissing does.</p>",
+"<p><strong>Fun Fact:</strong> Chocolate has over 600 flavor compounds, while red wine has just 200.</p>", "<p><strong>Fun Fact:</strong>.</p>"]
 };
 
 function displayQandA() {
@@ -17,32 +19,31 @@ function displayQandA() {
 
 displayQandA(currentIndex);
 
+console.log("current question is " + quiz.Q[currentIndex]);
+
+var userAnswer = 0;
+
 $('.quizCard').click(function() {
    var userAnswer = $(this).parent('li');
+   console.log("userAnswer is " + userAnswer);
 });
 
-/*  $('input[type="submit"]').click(function() {
-       // assign text input to a variable
-       var textInput = $('input[name="new-area-item"]', $(this).parent('form') );
-       var newItem = textInput.val();
-     }
-*/
+function checkAnswer() {
+    if (userAnswer == quiz.A[currentIndex].correctAns) {
+        score++;
+    }
+    else {
+        $(quiz.A[currentIndex].correctAns).prepend("Oops. ");
+    }
+}
 
-console.log(quiz.Q[currentIndex]);
-console.log("userAnswer is " + quiz.Q[currentIndex]);
+checkAnswer();
 
 $('.nextQuestion').click(function() {
     currentIndex++;
 });
 
-function checkAnswer(userAnswer) {
-    if (userAnswer == quizA[i].correctAns) {
-        score++;
-    }
-    else {
-        $( "Oops. ").prepend(quiz.A[currentIndex].correctAns);
-    }
-}
+console.log("currentIndex is " + currentIndex);
 
   // $('.fa-check').click(function () {
   // var chosenId = $(this).parent('li').attr('id');
@@ -55,7 +56,12 @@ function checkAnswer(userAnswer) {
   // populate the HTML with the question.
   // loop through all answers for the question & for every answer:
   // each answer : <li id = "answer_index"><i class = "fa-check"></i></li>
-
+  /*  $('input[type="submit"]').click(function() {
+         // assign text input to a variable
+         var textInput = $('input[name="new-area-item"]', $(this).parent('form') );
+         var newItem = textInput.val();
+       }
+  */
 /*
 display on quizCard ("Your score was " + score + " out of 5.")
 */
