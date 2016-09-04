53b9c2b8ced394dbb1a9657dbe591e2d924c6e30
lynnerw <larwitter@gmail.com>
2016-09-04 17:25:20 -0400

    implemented LESS, cleaned upclasses and ids, added some borders

--- js/quiz.js
+++ js/quiz.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
   var quiz = {
       /* declare object quiz, with properties Question, Answer, Correct Answer Value per ordered list  */
-      Q:["<p>Cocoa is:</p>", "<p>People have enjoyed treats made from cocoa since:</p>", "<p>Before cocoa beans are made into chocolate they are: </p>", "<p>The following statement about chocolate is true: </p>", "<p>Studies have shown the benefits of chocolate include: </p>"],
+      Q:["<p><b>Cocoa is:</b></p>", "<p><b>People have enjoyed treats made from cocoa since:</b></p>", "<p><b>Before cocoa beans are made into chocolate they are:</b></p>", "<p><b>The following statement about chocolate is true:</b></p>", "<p><b>Studies have shown the benefits of chocolate include:</b></p>"],
       A:["<li>A small town in Florida</li><li>An ingredient required to make chocolate</li><li>Apple’s object­-oriented API for OS X</li><li>All of the above</li>", "<li>The height of the Roman empire, about 100 A.D.</li><li>Europeans found it in South America in 1517</li><li>The Late Bronze Age, about 2000 B.C.</li><li>The Hershey chocolate factory was built in 1879</li>", "<li>Fermented</li><li>Broiled</li><li>Soaked</li><li>Boiled</li>", "<li>It's always been enjoyed as a candy bar.</li><li>Napolean drank 50 cups of chocolate daily.</li><li>We buy 58 million pounds of it for Valentine’s Day.</li><li>It cannot be sold legally in North Korea.</li>", "<li>Anti-aging properties</li><li>Improved LDL cholesterol levels</li><li>Improved athletic performance</li><li>Better scores on quizzes about chocolate</li>"],
       correctAnsValue:[4, 3, 1, 3, 2]
   };
@@ -20,13 +20,15 @@ $(document).ready(function() {
   /* define function to user answer against correct answer value and display feedback */
   function checkAnswer(userAnswer,currentIndex) {
     if (parseInt(userAnswer) == quiz.correctAnsValue[currentIndex]) {
-        $("<p><br>You're right!</p>").appendTo('.qAnda>ol');
+        $('#feedback').append("You're right!");
         score++;
     } else {
-        $("<p><br>Ooops; believe it or not, the answer is # " + (quiz.correctAnsValue[currentIndex]) + "!</p>").appendTo('.qAnda>ol');
+        $('#feedback').append("Ooops; believe it or not, the answer is # " + (quiz.correctAnsValue[currentIndex]) + "!");
     }
   }
+
   $('#final').hide();
+  $('#tryAgain').hide();
 
   doWork();
 
@@ -47,27 +49,29 @@ $(document).ready(function() {
         // allow user to select "next question" before displaying next Q and A pair
         $('.nextQuestion').on('click', function() {
             currentIndex++;
-            $('input[type="text"]').val('');
+            $('#feedback').empty();
+            $('input[type="number"]').val('');
             $('.nextQuestion').off('click');
             doWork();
-      });
+        });
     } else {
         $('.nextQuestion').off('click');
         $('#quizCard').hide();
         $('#final').show();
-        $("<p>You answered " + score + " out of 5 correctly.</p>" + "<p>Not too bad!</p>").appendTo('.score');
-        $("<p>Try again</p>").appendTo('.tryAgain');
+        $('#final').append("You answered " + score + " out of 5 correctly. Not too bad!");
+        $('#tryAgain').show();
+        $('#tryAgain').append("Try again?");
     }
-  }
 
-  $('.tryAgain').click(function() {
+    $('#tryAgain').click(function() {
         score = 0;
-        $('.score').empty();
-        $('.tryAgain').empty();
         userAnswer = 0;
         currentIndex = 0;
-
         $('#final').hide();
+        $('#tryAgain').hide();
+
+
         doWork();
-      });
+    });
+  }
 });
